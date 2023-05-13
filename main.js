54ea590c208c7b563487ac38fd0a5ffbce860a01
David Fumero <djfumero18@gmail.com>
2023-05-13 13:34:28 -0400

    Add function to play a round

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ function getComputerChoice() {
   return choices[randomNum]
 }
 
+playerChoice = playerChoice.toLowerCase()
+computerChoice = getComputerChoice().toLowerCase()
+
+// play a round and display results
+function playRound(playerChoice, computerChoice) {
+  console.log(`Player: ${playerChoice}`)
+  console.log(`Computer: ${computerChoice}`)
+
+  if (playerChoice == computerChoice) {
+    console.log("It's a tie!")
+  } else if ((playerChoice == "rock" && computerChoice == "scissors") || 
+             (playerChoice == "paper" && computerChoice == "rock") ||
+             (playerChoice == "scissors" && computerChoice == "paper")) {
+    console.log("You win!")
+  } else {
+    console.log("You lose!")
+  }
+}
+
+playRound(playerChoice, computerChoice)
